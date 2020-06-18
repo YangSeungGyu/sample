1ea3b5f6b7af66928e266bb1fe22a5b467f6a2d1
p190841 <p190841@SKMNS-p190841>
2020-06-18 17:12:26 +0900

    #초기설정  - aop

---
+++ src/main/java/com/sample/www/common/aop/AOPContext.java
@@ -0,0 +1,17 @@
+package com.sample.www.common.aop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AOPContext {
+
+	@Bean(name = "logAOP")
+	public LogAOP getLogAOP() {
+		return new LogAOP();
+	}
+
+}
