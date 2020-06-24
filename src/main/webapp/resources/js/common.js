d3010f7f7d01b826be4121c9b734ee1659a35e0c
p190841 <p190841@SKMNS-p190841>
2020-06-24 13:39:05 +0900

    #정규표현

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -4,5 +4,27 @@ common = {
 	},
 	test2 : function() {
 		alert('test1')
+	},
+	pttrnTest : function(msg) {
+		var result = false;
+		//첫번째가 대문자N으로 시작하고 그뒤에 10자리 숫자
+		var rgEx = /(N)(\d{10})$/g
+		result =  rgEx.test(msg);
+		return result;
+	},
+	pttrnMobl : function(num) {
+		var result = false;
+		//첫번째가 대문자N으로 시작하고 그뒤에 10자리 숫자
+		var rgEx = /(01[016789])(\d{4}|\d{3})\d{4}$/g;
+		result =  rgEx.test(num);
+		return result;
+	},
+	pttrnNum : function(num) {
+		var result = false;
+		//첫번째가 대문자N으로 시작하고 그뒤에 10자리 숫자
+		var rgEx = /[^0-9]/g;
+		result =  rgEx.test(num);
+		return result;
 	}
+	
 };
